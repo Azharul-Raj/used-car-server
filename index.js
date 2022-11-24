6a7a202b0fc7a01b1b564b387f6c6a14b40cdcdc
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-11-24 07:50:02 +0600

    category api created

--- index.js
+++ index.js
@@ -19,20 +19,24 @@ app.listen(port, () => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dnsrj7s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
+
 const connectDB = async () => {
     client.connect()
     console.log('db connected');
 }
 connectDB();
-const categoryList = client.db('used_car_zone').collection('categories')
+const categoryList = client.db('used_car_zone').collection('categories');
+const brandsList = client.db('used_car_zone').collection('category');
 
 app.get('/categories', async (req, res) => {
     const categories = await categoryList.find({}).toArray();
     res.send(categories);
 })
-app.post('/categories', async (req, res) => {
-    const info = req.body
-    const result =await categoryList.insertOne(info)
-    res.json(result)
+// 
+app.get('/category/:id', async (req, res) => {
+    const id=parseInt(req.params.id)
+    console.log(typeof id);
+    const query={category_id:id}
+    const brandCategory = await brandsList.find(query).toArray();
+    res.send(brandCategory)
 })
